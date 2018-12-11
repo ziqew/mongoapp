6408e98aa6dba04fbe2b69a1fea4cf562b306b91
keda <kdyq@promote.cache-dns.local>
2018-12-11 19:09:27 +0800

    change file

--- index.js
+++ index.js
@@ -2,9 +2,80 @@
 
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
-
+var fs = require('fs');
+const {promisify} = require('util');
+//var getAreaCodeList=require('./getAreaCodeList');
 // Connection URL
 var url = 'mongodb://hello:hello@192.168.100.100:27017/proj_db';
+
+
+function getAreaCodeList() {
+  let areaCodeList;
+  return new Promise(function (resolve) {
+    fs.readFile('area_code.js', 'utf8', function (err, data) {
+      if (err) throw err;
+      areaCodeList = JSON.parse(data);
+      resolve(areaCodeList);
+    });
+  });
+
+
+}
+
+MongoClient.connect(url, function (err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  db.collection('DeviceTree', function (err, collection) {
+    const deviceTreeFindOneAsync = promisify(db.collection('DeviceTree').findOne);
+    getAreaCodeList().then(function (areaCodeList) {
+      for (var key in areaCodeList) {
+        if (areaCodeList.hasOwnProperty(key)) {
+          let code = areaCodeList[key].code;
+          let name = areaCodeList[key].name;
+          let treeNode = null;
+          if (code.endsWith('0000')) {
+            //省或者直辖市
+            treeNode = {};
+            treeNode.nodeType = '1';
+            treeNode.civilCode = code;
+            treeNode.nodeName = name;
+            treeNode.parentId = null;
+          } else if (code.endsWith('00') && code.substring(2, 4) !== '00') {
+            //市
+            treeNode = {};
+            treeNode.nodeType = '1';
+            treeNode.civilCode = code;
+            treeNode.nodeName = name;
+            let parentNode = db.collection('DeviceTree').findOne({ civilCode: code.substring(0, 2) + '0000' });
+            treeNode.parentId = parentNode.id;
+            treeNode.ancestors = [parentNode.id];
+          } else {
+            //区或者县级市
+            treeNode = {};
+            treeNode.nodeType = '1';
+            treeNode.civilCode = code;
+            treeNode.nodeName = name;
+            let parentNode = db.collection('DeviceTree').findOne({ civilCode: code.substring(0, 4) + '00' });
+            let grandParentNode = db.collection('DeviceTree').findOne({ civilCode: code.substring(0, 2) + '0000' });
+            treeNode.parentId = parentNode.id;
+            treeNode.ancestors = [grandParentNode.id, parentNode.id];
+          }
+
+          if(treeNode!==null){
+             collection.insert(treeNode);
+          }
+        }
+      }
+    });
+
+  });
+  db.close();
+});
+
+
+
+
+
 
 /**
  * docker exec -it mongo_mongo_1 bash
@@ -25,22 +96,30 @@ db.createUser(
 )
  */
 // Use connect method to connect to the server
+
+/**
+ * 
+
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
   db.collection('Persons', function (err, collection) {
-        
-    collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-    collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-    collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+    
+    console.log("insert start time:" + Date.now());
+    for(let i=0; i < 10000;i++){
+      collection.insert({ id: i, firstName: 'Node' + i });
+    }
+    console.log("insert end time:" + Date.now());
     
     
-
+    console.log("query start time:" + Date.now());
     db.collection('Persons').count(function (err, count) {
         if (err) throw err;
-        
         console.log('Total Rows: ' + count);
+        console.log("query end time:" + Date.now());
     });
+    
 });
   db.close();
 });
+ */
